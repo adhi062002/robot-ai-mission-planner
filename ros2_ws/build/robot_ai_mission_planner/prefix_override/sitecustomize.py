8d16219a5ac36759bd9ec342ad54d1a8b63eb288
Adithya <adithya062002@hmail.com>
2026-06-30 19:39:31 +0530

    Ignore ROS2 build artifacts

--- ros2_ws/build/robot_ai_mission_planner/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/augustin/robot-ai-mission-planner/ros2_ws/install/robot_ai_mission_planner'
