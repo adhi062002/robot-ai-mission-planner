1cd006d2d2f785b533e689b9ccfcf7daa225c599
adhi062002 <adithya062002@gmail.com>
2026-07-01 10:13:00 +0530

    Phase 7: Improve mission executor for multi-action support and waypoint navigation

--- ros2_ws/src/robot_ai_mission_planner/robot_ai_mission_planner/executor/mission_executor.py
+++ ros2_ws/src/robot_ai_mission_planner/robot_ai_mission_planner/executor/mission_executor.py
@@ -10,7 +10,40 @@ class MissionExecutor:
 
         print("\n========== Mission Executor ==========")
 
-        action = mission["actions"][0]
+        for action in mission["actions"]:
+
+            if action["type"] == "follow_route":
+
+                route = self.load_route(action["route"])
+
+                if route is None:
+                    continue
+
+                print("--------------------------------------")
+                print(f"Route : {route['name']}")
+                print(f"Laps  : {action['laps']}")
+                print(f"Speed : {action['speed']}")
+                print("--------------------------------------")
+
+                print("\nWaypoints\n")
+
+                for index, waypoint in enumerate(route["waypoints"], start=1):
+
+                    print(
+                        f"Navigating to Waypoint {index}: "
+                        f"x={waypoint[0]}, y={waypoint[1]}"
+                    )
+
+                    # Placeholder for Nav2
+                    print("Goal reached\n")
+
+            else:
+
+                print(f"\n[WARNING] Unknown action: {action['type']}")
+
+        print("\nMission Complete")
+
+    def load_route(self, route_name):
 
         package_share = get_package_share_directory(
             "robot_ai_mission_planner"
@@ -19,29 +52,20 @@ class MissionExecutor:
         filename = os.path.join(
             package_share,
             "missions",
-            f"{action['route']}.json"
+            f"{route_name}.json"
         )
 
         try:
+
             with open(filename) as f:
-                route = json.load(f)
+                return json.load(f)
 
         except FileNotFoundError:
+
             print(f"\n[ERROR] Route file not found: {filename}")
-            return
+            return None
 
         except json.JSONDecodeError:
+
             print(f"\n[ERROR] Invalid JSON in: {filename}")
-            return
-
-        print("--------------------------------------")
-        print(f"Route : {route['name']}")
-        print(f"Laps  : {action['laps']}")
-        print(f"Speed : {action['speed']}")
-        print("--------------------------------------")
-        print("\nWaypoints")
-
-        for waypoint in route["waypoints"]:
-            print(waypoint)
-
-        print("\nMission Complete")
+            return None
